fdbdcd9661037729878f7995ccd2f8caf0741cce
pedro <pdrovncs@gmail.com>
2026-03-29 15:15:07 -0300

    refatorando tp1

--- src/main/java/org/infnet/guildaApiTP1/dto/CriarAventureiroRequest.java
+++ src/main/java/org/infnet/guildaApiTP1/dto/CriarAventureiroRequest.java
@@ -6,10 +6,15 @@ import org.infnet.guildaApiTP1.enums.ClasseEnum;
 public record CriarAventureiroRequest(
         @NotBlank(message = "Nome deve ser informado")
         String nome,
+
         @NotNull(message = "Classe deve ser informada corretamente.")
         ClasseEnum classe,
+
         @NotNull(message = "Nível deve ser informado.")
         @Min(message = "Nível informado deve ser no mínimo 1", value = 1)
-        Integer nivel
-        ) {
+        Integer nivel,
+
+        @NotNull(message = "Usuário responsável deve ser informado.")
+        Long usuarioResponsavelId
+) {
 }
--- src/main/java/org/infnet/guildaApiTP1/service/AventureiroService.java
+++ src/main/java/org/infnet/guildaApiTP1/service/AventureiroService.java
@@ -1,12 +1,17 @@
 package org.infnet.guildaApiTP1.service;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.infnet.guildaApiTP1.dto.*;
 import org.infnet.guildaApiTP1.enums.ClasseEnum;
 import org.infnet.guildaApiTP1.exceptions.EntityNotFoundException;
+import org.infnet.guildaApiTP1.model.audit_schema.Usuario;
 import org.infnet.guildaApiTP1.model.aventura_schema.Aventureiro;
+import org.infnet.guildaApiTP1.model.aventura_schema.Companheiro;
 import org.infnet.guildaApiTP1.repository.AventureiroRepository;
 import org.infnet.guildaApiTP1.repository.ParticipacaoEmMissaoRepository;
+import org.infnet.guildaApiTP1.repository.UsuarioRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +21,94 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AventureiroService {
     private final AventureiroRepository aventureiroRepository;
+
     private final ParticipacaoEmMissaoRepository participacaoEmMissaoRepository;
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public AventureiroDTO criarAventureiro(CriarAventureiroRequest dto) {
+        Usuario usuarioResponsavel = usuarioRepository.findById(dto.usuarioResponsavelId())
+                .orElseThrow(() -> new EntityNotFoundException("Usuário responsável não encontrado"));
+
+        Aventureiro aventureiro = new Aventureiro();
+        aventureiro.setNome(dto.nome());
+        aventureiro.setClasse(dto.classe());
+        aventureiro.setNivel(dto.nivel());
+        aventureiro.setAtivo(true);
+        aventureiro.setCompanheiro(null);
+        aventureiro.setUsuarioResponsavel(usuarioResponsavel);
+        aventureiro.setOrganizacao(usuarioResponsavel.getOrganizacao());
+
+        Aventureiro salvo = aventureiroRepository.save(aventureiro);
+
+        return new AventureiroDTO(
+                salvo.getId(),
+                salvo.getNome(),
+                salvo.getClasse(),
+                salvo.getNivel(),
+                salvo.isAtivo()
+        );
+    }
+
+    @Transactional
+    public AventureiroDTO atualizarAventureiro(Long id, AtualizarAventureiroRequest dto) {
+        Aventureiro aventureiro = aventureiroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Aventureiro não encontrado"));
+
+        aventureiro.setNome(dto.nome());
+        aventureiro.setClasse(dto.classe());
+        aventureiro.setNivel(dto.nivel());
+
+        Aventureiro salvo = aventureiroRepository.save(aventureiro);
+
+        return new AventureiroDTO(
+                salvo.getId(),
+                salvo.getNome(),
+                salvo.getClasse(),
+                salvo.getNivel(),
+                salvo.isAtivo()
+        );
+    }
+
+    @Transactional
+    public void encerrarVinculo(Long id) {
+        Aventureiro aventureiro = aventureiroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Aventureiro não encontrado"));
+
+        aventureiro.setAtivo(false);
+        aventureiroRepository.save(aventureiro);
+    }
+
+    @Transactional
+    public void renovarVinculo(Long id) {
+        Aventureiro aventureiro = aventureiroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Aventureiro não encontrado"));
+
+        aventureiro.setAtivo(true);
+        aventureiroRepository.save(aventureiro);
+    }
+
+    @Transactional
+    public void definirCompanheiro(Long id, CriarCompanheiroRequest dto) {
+        Aventureiro aventureiro = aventureiroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Aventureiro não encontrado"));
+
+        Companheiro companheiro = new Companheiro();
+        companheiro.setNome(dto.nome());
+        companheiro.setEspecie(dto.especie());
+        companheiro.setIndiceLealdade(dto.lealdade());
+
+        aventureiro.setCompanheiro(companheiro);
+        aventureiroRepository.save(aventureiro);
+    }
+
+    @Transactional
+    public void removerCompanheiro(Long id) {
+        Aventureiro aventureiro = aventureiroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Aventureiro não encontrado"));
+        aventureiro.setCompanheiro(null);
+    }
 
     public Page<AventureiroDTO> listarAventureirosComFiltros(Boolean ativo, ClasseEnum classe, Integer nivelMinimo, Pageable pageable) {
         return aventureiroRepository.buscarComFiltros(ativo, classe, nivelMinimo, pageable);
@@ -69,4 +161,6 @@ public class AventureiroService {
                 ultimaMissao
         );
     }
+
+
 }
